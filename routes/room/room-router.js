887a1effc04c4dbb2889ce1a0e25655ae85cd3e4
Elias <worldlolxd121@gmail.com>
2024-03-21 20:56:23 +0300

    feat: add routers auth, message, user, room, userInRoom

---
+++ routes/room/room-router.js
@@ -0,0 +1,24 @@
+import express from 'express'
+const router = express.Router()
+
+router.post('/room', async (req, res) => {
+    
+})
+
+router.get('/room/:id', async (req, res) => {
+
+})
+
+router.get('/room', async (req, res) => {
+
+})
+
+router.put('/room', async (req, res) => {
+
+})
+
+router.delete('/room', async (req, res) => {
+    
+})
+
+export default router;
