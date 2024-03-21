ce7c1c98f893947ce196b25aef18d7f5e6b4aac3
Elias <worldlolxd121@gmail.com>
2024-03-21 19:04:21 +0300

    feat: install cors, morgan, helmet

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
-const PORT = process.env.PORT | 3001
+const PORT = process.env.PORT | 3001;
 
-const app = express()
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.listen(PORT, () => {
-    console.log(`Application is running on: ${PORT}`)
+    console.log(`Application is running on: ${PORT}`);
 })
